70574d02d60e3dd8150c05b22a020a5db75da430
besser <besser9009@mail.ru>
2023-04-19 19:55:51 +0300

    add Num02

---
+++ Num02/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+// int [] NewArray()
+// {
+//     int n = 1;
+//     int[] array = new int[3];
+//     for (int i = 0; i < 3; i++)
+//     {
+//         Console.Write($"Введите длину {n} стороны: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//         n++;
+//     }
+//     return array;
+// }
+// int[] array = NewArray();
+// System.Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.Write("Введите три числа (через запятую): ");
+int[] array = Console.ReadLine().Split(',').Select(num => int.Parse(num)).ToArray();
+void Triangle(int[] array)
+{
+    string msg = (array[0] < array[1] + array[2] && array[1] < array[0] + array[2] && array[2] < array[1] + array[0]) ? "Yes, it's triangle" : "No... It's not triangle";
+    Console.WriteLine(msg);
+}
+Triangle(array);
