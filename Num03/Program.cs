8a32320bf514b872d4625347c474eee452037655
besser <besser9009@mail.ru>
2023-04-21 12:53:20 +0300

    add Num03

---
+++ Num03/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int GetLength(int num)
+{
+    int length = 1;
+    for (; num > 1; length++)
+    {
+        num = num / 2;
+    }
+    return length;
+}
+int length = GetLength(num);
+int[] GetArray(int num)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length - 1; i++)
+    {
+        array[i] = num % 2;
+        num = num / 2;
+    }
+    array[length - 1] = 1;
+    Array.Reverse(array);
+    return array;
+}
+int[] array = GetArray(num);
+Console.WriteLine($"{num} = {String.Join(" ", array)}");
